aab55a3b13839e5374e7be6b4c00ef080ea9b04b
connierearden <romanov.konst96@gmail.com>
2021-11-05 13:25:55 +0300

    Initial commit

---
+++ src/main/java/seafoodexchange/SeafoodExchangeApplication.java
@@ -0,0 +1,13 @@
+package seafoodexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeafoodExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeafoodExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seafoodexchange/SeafoodExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package seafoodexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeafoodExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
